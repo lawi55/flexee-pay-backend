f9265e5827ea2926fe4b6e0215b48a7dabe219f8
Louay Sejine <102327128+lawi55@users.noreply.github.com>
2025-09-29 23:14:17 +0100

    Update otpUtils.js

--- utils/otpUtils.js
+++ utils/otpUtils.js
@@ -1,6 +1,5 @@
 const crypto = require("crypto");
 
 exports.generateOTP = () => {
-  // return crypto.randomInt(111111, 111111).toString(); // Génère un OTP à 6 chiffres
-  return "111111"
+  return crypto.randomInt(111111, 111111).toString(); // Génère un OTP à 6 chiffres
 };
